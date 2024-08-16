5b9f97c7278f04922ed201eaa2d51bf626ce042c
Bhuwan Chaudhary <bhuwan.chaudhary@kotuko.it>
2024-08-16 18:18:39 +0545

    demo bookstore app

--- clusus-task/bookstore/src/main/java/com/bookStore/controller/BookController.java
+++ clusus-task/bookstore/src/main/java/com/bookStore/controller/BookController.java
@@ -1,5 +1,7 @@
 package com.bookStore.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,26 +12,47 @@ import com.bookStore.entity.Book;
 import com.bookStore.entity.MyBookList;
 import com.bookStore.service.BookService;
 import com.bookStore.service.MyBookListService;
+import com.bookStore.utils.NetworkUtils;
 
 import java.util.*;
 
 @Controller
 public class BookController {
 
+    private static final Logger logInfo = LoggerFactory.getLogger(BookController.class);
 
 	@Autowired
 	private BookService service;
 	
 	@Autowired
 	private MyBookListService myBookService;
+
+     // Utility methods to get hostname and IP address
+     private String getHostName() {
+        return NetworkUtils.getHostName();
+    }
+
+    private String getHostAddress() {
+        return NetworkUtils.getHostAddress();
+    }
 	
 	@GetMapping("/")
 	public String home() {
+        String hostName = getHostName();
+        String hostAddress = getHostAddress();
+
+        logInfo.info("Welcome to BookStore Application !!!,  Enjoy the application !!!!");
+        logInfo.debug("ClususTask JavaApplication in Dockercompose and sending logs to ELK Stack");
+        logInfo.info("Running on Hostname: {} with IP Address: {}", hostName, hostAddress);
 		return "home";
 	}
 	
 	@GetMapping("/book_register")
 	public String bookRegister() {
+        String hostName = getHostName();
+        String hostAddress = getHostAddress();
+        logInfo.info("Registering New BookList... ");
+        logInfo.info("Registered from: Running Hostname: {} with IP Address: {}", hostName, hostAddress);
 		return "bookRegister";
 	}
 	
@@ -39,12 +62,18 @@ public class BookController {
 //		ModelAndView m=new ModelAndView();
 //		m.setViewName("bookList");
 //		m.addObject("book",list);
+        String hostName = getHostName();
+        String hostAddress = getHostAddress();
+        logInfo.info("Available books...");
+        logInfo.info("Available books from: Running Hostname: {} with IP Address: {}", hostName, hostAddress);
 		return new ModelAndView("bookList","book",list);
 	}
 	
 	@PostMapping("/save")
 	public String addBook(@ModelAttribute Book b) {
+        logInfo.info("Saving books...");
 		service.save(b);
+        
 		return "redirect:/available_books";
 	}
 	@GetMapping("/my_books")
@@ -59,6 +88,10 @@ public class BookController {
 		Book b=service.getBookById(id);
 		MyBookList mb=new MyBookList(b.getId(),b.getName(),b.getAuthor(),b.getPrice());
 		myBookService.saveMyBooks(mb);
+        String hostName = getHostName();
+        String hostAddress = getHostAddress();
+        logInfo.info("My BookList: {}",id);
+        logInfo.info("BookList from: Running Hostname: {} with IP Address: {}", hostName, hostAddress);
 		return "redirect:/my_books";
 	}
 	
@@ -66,11 +99,19 @@ public class BookController {
 	public String editBook(@PathVariable("id") int id,Model model) {
 		Book b=service.getBookById(id);
 		model.addAttribute("book",b);
+        String hostName = getHostName();
+        String hostAddress = getHostAddress();
+        logInfo.info("Edited BookList ID: {}",id);
+        logInfo.info("Edited from: Running Hostname: {} with IP Address: {}", hostName, hostAddress);
 		return "bookEdit";
 	}
 	@RequestMapping("/deleteBook/{id}")
 	public String deleteBook(@PathVariable("id")int id) {
 		service.deleteById(id);
+        String hostName = getHostName();
+        String hostAddress = getHostAddress();
+        logInfo.info("Deleted BookList ID: {}",id);
+        logInfo.info("Deleted from: Running Hostname: {} with IP Address: {}", hostName, hostAddress);
 		return "redirect:/available_books";
 	}
 	
---
+++ clusus-task/bookstore/src/main/java/com/bookStore/utils/NetworkUtils.java
@@ -0,0 +1,23 @@
+package com.bookStore.utils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class NetworkUtils {
+
+    public static String getHostName() {
+        try {
+            return InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            return "unknown";
+        }
+    }
+
+    public static String getHostAddress() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            return "unknown";
+        }
+    }
+}
